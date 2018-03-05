b9f93b7910b8295080b1be92de804760298593fa
HE CHAOYI <peecyhe@ats.asmpt.com>
2018-03-05 09:11:55 +0800

    Worked out hw04 Q1-Q4

---
+++ hw/hw04/hw04/hw04.py
@@ -0,0 +1,206 @@
+HW_SOURCE_FILE = 'hw04.py'
+
+###############
+#  Questions  #
+###############
+
+def intersection(st, ave):
+    """Represent an intersection using the Cantor pairing function."""
+    return (st+ave)*(st+ave+1)//2 + ave
+
+def street(inter):
+    return w(inter) - avenue(inter)
+
+def avenue(inter):
+    return inter - (w(inter) ** 2 + w(inter)) // 2
+
+w = lambda z: int(((8*z+1)**0.5-1)/2)
+
+def taxicab(a, b):
+    """Return the taxicab distance between two intersections.
+
+    >>> times_square = intersection(46, 7)
+    >>> ess_a_bagel = intersection(51, 3)
+    >>> taxicab(times_square, ess_a_bagel)
+    9
+    >>> taxicab(ess_a_bagel, times_square)
+    9
+    """
+    "*** YOUR CODE HERE ***"
+    dist_street = abs(street(a) - street(b))
+    dist_avenue = abs(avenue(a) - avenue(b))
+    return dist_avenue + dist_street
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    return [round(element ** 0.5) for element in s if round(element ** 0.5) == element ** 0.5]
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    else:
+        return g(n-1) + 2*g(n-2) + 3*g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    #Need to be further optimized
+    if n <= 3:
+        return n
+    else:
+        x1, x2, x3, k = 1, 2, 3, 4
+        while k <= n:
+            g = 3 * x1 + 2 * x2 + x3
+            x1, x2, x3, k = x2, x3, g, k + 1
+        return g
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def direction(n):
+        if n < 7:
+            return 1
+        else:
+            if has_seven(n) or n % 7 == 0:
+                return -direction(n-1)
+            else:
+                return direction(n-1)
+
+    if n <= 7:
+        return n
+    else:
+        return pingpong(n-1) + direction(n-1)
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    "*** YOUR CODE HERE ***"
+    def count_partitions(n, m):
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 0
+        elif m == 1:
+            return 0
+        else:
+            return count_partitions(n-m, m) + count_partitions(n, m/2)
+
+###################
+# Extra Questions #
+###################
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
